92aa486fcc65823b8c11312b895815c8705108e7
Frank Pier <frank.pier@rwth-aachen.de>
2026-06-29 11:40:50 +0200

    expertino_rl: added logging

--- expertino_rl/expertino_rl/expertino_env.py
+++ expertino_rl/expertino_rl/expertino_env.py
@@ -23,17 +23,17 @@ class ExpertinoEnv(CXRLGym):
         self.expertino_dir = get_package_share_directory('expertino_rl')
         self.start_refbox_server = ActionServer(self.node, StartRefbox, 'cx_rl_node/start_refbox', self.start_refbox_callback)
         self.stop_refbox_server = ActionServer(self.node, StopRefbox, 'cx_rl_node/stop_refbox', self.stop_refbox_callback)
+
+        self.log_file = os.path.join(self.node.log_dir, "rewards.txt")
         
 
     def step(self, action):
-        with open("cxrl-rcll-log-episode-reward.txt", 'a+') as f:
-            f.write(f"{self.action_dict[action]} \n")
         state, reward, done, truncated, info = super().step(action)
         self.reward_in_episode += reward
         return state, reward, done, truncated, info
     
     def reset(self, seed: int = None, options: dict[str, any] = None):
-        with open("cxrl-rcll-log-episode-reward.txt", 'a+') as f:
+        with open(self.log_file, 'a+') as f:
             f.write(f"{self.reward_in_episode} \n")
         self.node.get_logger().info(f"Episode {self.episode_number}.")
         self.episode_number += 1
@@ -42,63 +42,13 @@ class ExpertinoEnv(CXRLGym):
         state, info = super().reset(seed=seed)
         
         return (state,info)
-    """
-    def generate_action_space(self):
-        self.node.get_logger().info("Generating action space...")
-        action_space =  ["spawn-and-transport#o1",
-                         "transport#o1",
-                         "base-transport#o1",
-                         "spawn-and-transport#o2",
-                         "transport#o2",
-                         "base-transport#o2",
-                         "spawn-and-transport#o3",
-                         "transport#o3",
-                         "base-transport#o3",
-                         "spawn-and-transport#o4",
-                         "transport#o4",
-                         "base-transport#o4",
-                         "spawn-and-transport#o5",
-                         "transport#o5",
-                         "base-transport#o5",
-                         "spawn-and-transport#o6",
-                         "transport#o6",
-                         "base-transport#o6",
-                         "spawn-and-transport#o7",
-                         "transport#o7",
-                         "base-transport#o7",
-                         "spawn-and-transport#o8",
-                         "transport#o8",
-                         "base-transport#o8",
-                         "spawn-and-transport#o9",
-                         "transport#o9",
-                         "base-transport#o9",
-                         "spawn-and-transport#o10",
-                         "transport#o10",
-                         "base-transport#o10",
-                         "pay-with-carrier#rs1#black-carrier",
-                         "pay-with-carrier#rs2#black-carrier",
-                         "pay-with-carrier#rs1#grey-carrier",
-                         "pay-with-carrier#rs2#grey-carrier",
-                         "carrier-to-input#black-carrier#cs1",
-                         "carrier-to-input#black-carrier#cs2",
-                         "carrier-to-input#grey-carrier#cs1",
-                         "carrier-to-input#grey-carrier#cs2",
-                         "transport-to-slide#black-carrier#rs1",
-                         "transport-to-slide#black-carrier#rs2",
-                         "transport-to-slide#grey-carrier#rs1",
-                         "transport-to-slide#grey-carrier#rs2",
-                         "pay-from-bs#rs1",
-                         "pay-from-bs#rs2"
-                        ]       
-        return action_space
-    """
 
     def render(self):
         pass
     
     def start_refbox_callback(self, goal_handle):
         self.node.get_logger().info("Starting refbox...")
-        
+
         start_path = os.path.join(self.expertino_dir + "/scripts/start_refbox.sh")
         subprocess.call(['sh', start_path])
         
@@ -109,7 +59,6 @@ class ExpertinoEnv(CXRLGym):
         
     def stop_refbox_callback(self, goal_handle):
         self.node.get_logger().info("stopping refbox...")
-        
         stop_path = os.path.join(self.expertino_dir + "/scripts/stop_refbox.sh")
         subprocess.call(['sh', stop_path])
         
